59e519b365d7c0e1744bdd261a31d7e94e99ff69
mkouichi <k-m2011@hotmail.co.jp>
2020-05-13 18:32:14 +0900

    Add some links to the card and footer, disabled some links in the footer

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,19 @@
-//cognitoのログインURLを生成して、そこへ移動する（OAuth2）
-document.getElementById('loginBtn').addEventListener('click', () => {
-    //Cognitoがホストするログインページのドメイン。ユーザープールで設定したやつ。最後に\loginをつける。
-    const loginEndPoint = 'https://dev-mad-sumunara.auth.ap-northeast-1.amazoncognito.com/login';
-    const params = new URLSearchParams({
-        //implicitなのでresponse_typeはtoken
-        response_type: 'token',
-        client_id: '7ejl0oi8jli7874hudi420nl52',
-        scope: 'email',
-        redirect_uri: 'https://sumunara-app.herokuapp.com/sumunara_apps/address_form/',
-    });
-    location.href = `${loginEndPoint}?${params.toString()}`;
+// cognitoのログインURLを生成して、そこへ移動する（OAuth2）
+document.getElementById("loginBtn").addEventListener("click", e => {
+	e.preventDefault();
+
+	// Cognitoがホストするログインページのドメイン。ユーザープールで設定したやつ。最後に\loginをつける。
+	const loginEndPoint =
+		"https://sumunara-test1.auth.ap-northeast-1.amazoncognito.com/login";
+	const params = new URLSearchParams({
+		// implicitなのでresponse_typeはtoken
+		response_type: "token",
+		client_id: "476hj5kunmf68ab5maas3qmv53",
+		// state: "STATE",      // 「この値は、CSRF 攻撃を防ぐために、クライアントで使用する必要があります。オプションですが、強くお勧めします。」
+		scope: "email",
+		redirect_uri:
+			"https://sumunara-app.herokuapp.com/sumunara_apps/address_form/",
+	});
+
+	location.href = `${loginEndPoint}?${params.toString()}`;
 });
