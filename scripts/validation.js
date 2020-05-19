e23b3df9b9c4122b045bdf010a5e11f6989fadc2
mkouichi <k-m2011@hotmail.co.jp>
2020-05-19 22:36:37 +0900

    Add noise standards to details.html and add if statements to validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,53 +1,68 @@
 // Form blur event listeners
-document.getElementById("inputUserName").addEventListener("blur", validateName);
-document.getElementById("inputEmail").addEventListener("blur", validateEmail);
-document
-    .getElementById("inputPassword")
-    .addEventListener("blur", validatePassword);
-document
-    .getElementById("inputConfirmPassword")
-    .addEventListener("blur", confirmPassword);
+if (document.getElementById("inputUserName")) {
+	document
+		.getElementById("inputUserName")
+		.addEventListener("blur", validateName);
+}
+
+if (document.getElementById("inputEmail")) {
+	document
+		.getElementById("inputEmail")
+		.addEventListener("blur", validateEmail);
+}
+
+if (document.getElementById("inputPassword")) {
+	document
+		.getElementById("inputPassword")
+		.addEventListener("blur", validatePassword);
+}
+
+if (document.getElementById("inputConfirmPassword")) {
+	document
+		.getElementById("inputConfirmPassword")
+		.addEventListener("blur", confirmPassword);
+}
 
 function validateName() {
-    const name = document.getElementById("inputUserName");
-    const re = /^[a-zA-Z]{2,15}$/;
+	const name = document.getElementById("inputUserName");
+	const re = /^[a-zA-Z]{2,15}$/;
 
-    if (!re.test(name.value)) {
-        name.classList.add("is-invalid");
-    } else {
-        name.classList.remove("is-invalid");
-    }
+	if (!re.test(name.value)) {
+		name.classList.add("is-invalid");
+	} else {
+		name.classList.remove("is-invalid");
+	}
 }
 
 function validateEmail() {
-    const email = document.getElementById("inputEmail");
-    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+	const email = document.getElementById("inputEmail");
+	const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-    if (!re.test(email.value)) {
-        email.classList.add("is-invalid");
-    } else {
-        email.classList.remove("is-invalid");
-    }
+	if (!re.test(email.value)) {
+		email.classList.add("is-invalid");
+	} else {
+		email.classList.remove("is-invalid");
+	}
 }
 
 function validatePassword() {
-    const password = document.getElementById("inputPassword");
-    const re = /^(?!.*[\s])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+	const password = document.getElementById("inputPassword");
+	const re = /^(?!.*[\s])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
-    if (!re.test(password.value)) {
-        password.classList.add("is-invalid");
-    } else {
-        password.classList.remove("is-invalid");
-    }
+	if (!re.test(password.value)) {
+		password.classList.add("is-invalid");
+	} else {
+		password.classList.remove("is-invalid");
+	}
 }
 
 function confirmPassword() {
-    const password = document.getElementById("inputPassword");
-    const confirmPassword = document.getElementById("inputConfirmPassword");
+	const password = document.getElementById("inputPassword");
+	const confirmPassword = document.getElementById("inputConfirmPassword");
 
-    if (password.value !== confirmPassword.value) {
-        confirmPassword.classList.add("is-invalid");
-    } else {
-        confirmPassword.classList.remove("is-invalid");
-    }
+	if (password.value !== confirmPassword.value) {
+		confirmPassword.classList.add("is-invalid");
+	} else {
+		confirmPassword.classList.remove("is-invalid");
+	}
 }
